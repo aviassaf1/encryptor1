11b1467685ce14ccb0735e7fc9f39bd153c6da83
aviassaf <guy30aviassaf@gmail.com>
2017-05-27 13:43:11 +0300

    finish task2 fix and refactor task1

--- src/main/java/encryptor/Cmd.java
+++ src/main/java/encryptor/Cmd.java
@@ -4,11 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Cmd {
 
@@ -16,73 +13,73 @@ public class Cmd {
 		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		String path="";
-		boolean dec=false;
-		boolean enc=false;
-		boolean pathExist=false;
+		boolean chooseEncOrDec=false;
 		boolean done=false;
+		byte[] ans=new byte[0];
 		while(!done){
 			try {
 				System.out.println("hello, please choose enryption(E) or decription(D)");
-				while(!enc&!dec){
+				while(!chooseEncOrDec){
 					line=buffer.readLine();
-					if(line.equals("E"))
-						enc=true;
-					else if(line.equals("D"))
-						dec=true;
+					if(line.equals("E")){
+						chooseEncOrDec=true;
+						path=getFilePath();
+						ans=enc1(path);
+						done=true;
+					}
+					else if(line.equals("D")){
+						chooseEncOrDec=true;
+						path=getFilePath();
+						ans=dec1(path);
+						done=true;
+					}
 					else{
 						System.out.println("please choose only one of the options enryption(E) or decription(D)");
 					}
 				}
-				System.out.println("plese enter file path here:");
-				while(!pathExist){
-					path=buffer.readLine();
-					File f = new File(path);
-					if(f.exists() && !f.isDirectory()) { 
-					   pathExist=true;
-					}
-					else{
-						System.out.println("the path you enterd is not a correct file path, please try again:");
-					}
-				}
-				done=true;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		String ans="";
-		if(dec){
-			ans=dec1(path);
+		for(int i=0;i<ans.length;i++){
+			System.out.println(ans[i]);
 		}
-		else if(enc){
-			ans=enc1(path);
-		}
-		System.out.println(ans);
 	}
 
-	public static String enc1(String path) {
-		List<String> lines=new LinkedList<String>();
-		try {
-			lines = Files.readAllLines(Paths.get(path),StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static String getFilePath() throws IOException {
+		boolean pathExist=false;
+		String path="";
+		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("plese enter file path here:");
+		while(!pathExist){
+			path=buffer.readLine();
+			File f = new File(path);
+			if(f.exists() && !f.isDirectory()) { 
+			   pathExist=true;
+			}
+			else{
+				System.out.println("the path you enterd is not a correct file path, please try again:");
+			}
 		}
-		String ans="enc:";
-		for(int i=0;i<lines.size();i++){
-			ans=ans+lines.get(i)+"\n";
-		}
-		return ans;
+		return path;
 	}
-	public static String dec1(String path) {
-		List<String> lines=new LinkedList<String>();
+
+	private static byte[] enc1(String path) {
+		return getFileBytes(path);
+	}
+	private static byte[] dec1(String path) {
+		return getFileBytes(path);
+	}
+	private static byte[] getFileBytes(String path) {
+		byte[] byts=new byte[0];
 		try {
-			lines = Files.readAllLines(Paths.get(path),StandardCharsets.UTF_8);
+			byts = Files.readAllBytes(Paths.get(path));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		String ans="dec:";
-		for(int i=0;i<lines.size();i++){
-			ans=ans+lines.get(i)+"\n";
+		byte[] ans=new byte[byts.length];
+		for(int i=0;i<byts.length;i++){
+			ans[i]=byts[i];
 		}
 		return ans;
 	}
---
+++ src/main/java/encryptor/CmdTests.java
@@ -0,0 +1,14 @@
+package encryptor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CmdTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
